3bba57ae4ceb79c459ac423e7b16cee60000811a
Chris Todd <chriswilltodd@gmail.com>
2020-12-29 12:54:48 +1300

    streamlined pipeline to create new datasets

--- views/app.js
+++ views/app.js
@@ -41,105 +41,67 @@ client.connect(async (err, db) => {
 
     res.sendFile(path.join(__dirname + "/index/index.html"));
   })
-  
-  //Auto generate valid trips from realtime_raw 
-  app.get('/compare', async (req, res) => {  
+
+  app.get('/distinct', async (req, res) => {
+    //render page  
+    await collection.distinct("UUID", {}, {}, function (err, results) {
+      if (err) throw err;
+      console.log(results.length)
+      res.send(results);
+    })
+
+    await collection.find({}, {}).toArray((err, docs) => {
+      if (err) throw err;
+      console.log(docs.length)
+    })
+  })
+
+  //Auto generate valid trips from realtime_raw
+  //Overwrites: raw_w_routes
+  app.get('/compare', async (req, res) => {
     let dateToCheck = formDateArray();
-    console.log("dates", dateToCheck);
     let trips = dbo.collection("realtime_raw");
 
     let lookup = [
       {
-        "$match" : {
-          'date' : {"$in" : dateToCheck}
+        "$match": {
+          'date': { "$in": dateToCheck }
         }
       },
       {
-        "$lookup" : {
-          "from" : "routes",
-          "localField" : "route_id",
-          "foreignField" : "route_id",
-          "as" : "raw_w_route_id"
+        "$lookup": {
+          "from": "routes",
+          "localField": "route_id",
+          "foreignField": "route_id",
+          "as": "raw_w_route_id"
         }
       },
       {
-        "$match" : {
-          "raw_w_route_id.0.agency_id" : {"$in" : ["RTH", "HE", "GBT"]}
+        "$match": {
+          "raw_w_route_id.0.agency_id": { "$in": ["RTH", "HE", "GBT"] }
         }
       },
       {
-        "$out" : "raw_w_routes"
+        "$out": "raw_w_routes"
       }
     ]
 
     let options = {
       allowDiskUse: 1
     };
-    
+
     await trips.aggregate(lookup, options).toArray((err, docs) => {
       if (err) throw err;
-      console.log("Result: ", docs);
+      console.log("Collection raw_w_routes has been created!");
     })
   })
 
-  app.get('/distinct', async (req, res) => {
-    //render page  
-      await collection.distinct("UUID", {}, {}, function(err, results) {
-        if (err) throw err;
-        console.log(results.length)
-        res.send(results);
-      })
-  
-      await collection.find({}, {}).toArray((err, docs) => {
-        if (err) throw err;
-        console.log(docs.length)
-      })
-  })
-
-  app.get('/test', async (req, res) => {
-    let UUID = "20201214-1126203252-20201205123725_v95.82";
-    //render page  
-      await collection.find({"UUID" : UUID}, {}).toArray(async function(err, results) {
-        if (err) throw err;
-        console.log(results);
-        let stop_time_arrival = {
-          "delay" : 1,
-          "time" : 1,
-          "uncertainty" : 1
-        }
-        let each = {
-          "UUID": results[0].UUID,
-          "arrived?": results[0].arrived,
-          "stop_id": results[0].stop_id,
-          "stop_sequence": results[0].stop_sequence,
-          "direction_id": results[0].direction_id,
-          "route_id": results[0].route_id,
-          "date": results[0].date,
-          "start_time": results[0].start_time,
-          "trip_id": results[0].trip_id,
-          "vehicle_id": results[0].vehicle_id
-        };
-
-        let bulk = dbo.collection("realtime_raw").initializeUnorderedBulkOp();
-        bulk.find({
-          "UUID": each.UUID
-        }).updateOne({
-          "$set": each
-        });
-
-        bulk.execute(async function (err, updateResult) {
-          if (err) throw err;
-          console.log(err, updateResult);
-          await collection.find({"UUID" : each.UUID}, {}).toArray(async function(err, results) {
-            if (err) throw err;
-            res.send(results);
-          })
-        })
-      })
-  })
-  
+  //Test the trips that are currently without stop information
+  //Uses: raw_w_routes
+  //Overwrites: invalid_trips_buffer
   app.get("/completed_trips", async (req, res) => {
     let dateToCheck = formDateArray();
+    console.log(dateToCheck);
 
     collection = dbo.collection("raw_w_routes");
 
@@ -207,25 +169,25 @@ client.connect(async (err, db) => {
       },
       {
         "$group": {
-          "_id": { 
+          "_id": {
             "stop_id": "$stop_id"
           },
-          "count" : {"$sum" : 1},
-          "stop_time_arrival":  {"$push" : "$stop_time_arrival"},
-          "UUID":  {"$push" : "$UUID"},
-          "route_id" : {"$push" : "$route_id"}
+          "count": { "$sum": 1 },
+          "stop_time_arrival": { "$push": "$stop_time_arrival" },
+          "UUID": { "$push": "$UUID" },
+          "route_id": { "$push": "$route_id" }
         }
       },
       {
-        "$lookup" : {
-          "from" : "routes",
-          "localField" : "route_id",
-          "foreignField" : "route_id",
-          "as" : "routes"
+        "$lookup": {
+          "from": "routes",
+          "localField": "route_id",
+          "foreignField": "route_id",
+          "as": "routes"
         }
       },
       {
-        "$out" : "invalid_trips_buffer"
+        "$out": "invalid_trips_buffer"
       }
     ]
 
@@ -241,64 +203,166 @@ client.connect(async (err, db) => {
     await collection.aggregate(pipeline2, options).toArray((err, docs) => {
       if (err) throw err;
       console.log("No. unique combinations of missing stop info: ", docs.length);
-      console.log("Unique combinations: ");
-      console.log(docs);
+      console.log("The trips with missing information have been stored in invalid_trips_buffer.")
     })
   })
 
-  //Uses URL format string 
-  //articles?year=2016&month=1&day=19
-  app.get("/realtime_raw", (req, res) => {
-    let response = [];
-    let options = req.query === {} ? { "limit": 1 } : {};
+  // Join the calendar to the schedule information to form our UUID
+  // Uses: schedule_trips, filtered_trips
+  // Overwrites: filtered_trips
+  app.get("/generate_schedule_calendar", async (req, res) => {
+    let c1 = dbo.collection("schedule_trips");
 
-    collection.find(req.query, options).toArray((err, docs) => {
-      response.push("retrieved docs: ");
-      response.push(docs);
-      res.send(response);
-    });
+    //Ensure index is created
+    // :( cannot do because route_id is not unique
+    // await c1.createIndex({"route_id" : 1}, {unique:true});
+
+    //Add routes
+    let pipe = [
+      {
+        "$lookup": {
+          "from": "routes",
+          "localField": "route_id",
+          "foreignField": "route_id",
+          "as": "routes"
+        }
+      },
+      {
+        "$match": {
+          "routes.0.agency_id": { "$in": ["GBT", "HE", "RTH"] }
+        }
+      },
+      {
+        "$out": "filtered_trips"
+      }
+    ]
+
+    let c2 = dbo.collection("filtered_trips");
+
+    // await c2.createIndex({ "service_id": 1 }, { unique: true });
+
+    //Add calendar days
+    let pipe2 = [
+      {
+        "$lookup": {
+          "from": "calendar",
+          "localField": "service_id",
+          "foreignField": "service_id",
+          "as": "service_days"
+        }
+      },
+      {
+        "$out": "filtered_trips"
+      }
+    ]
+
+    let options = {
+      allowDiskUse: 1
+    };
+
+    c1.aggregate(pipe, options).toArray((err, docs) => {
+      if (err) throw err;
+      console.log("Added filtered route information to schedule_trips, written to filtered_trips");
+      c2.aggregate(pipe2, options).toArray((err, docs) => {
+        if (err) throw err;
+        console.log("Added filtered calendar information to filtered_trips, written to filtered_trips");
+      })
+    })
+  })
+
+  //Create the final collection of schedule information with a collection of trip ids
+  //Uses: filtered_trips
+  //Overwrites: final_trip_UUID_set
+  app.get("/generate_schedule_UUID", async (req, res) => {
+    let c1 = dbo.collection("filtered_trips");
+
+    // await c1.createIndex({ "service_days.start_date": 1 }, { unique: true });
+
+    let query = {"service_days.start_date": { "$gte": "2020-12-22T00:00:00.000Z" } }
+
+    let offsets = {
+      "tuesday": 0,
+      "wednesday": 1,
+      "thursday": 2,
+      "friday": 3,
+      "saturday": 4,
+      "sunday": 5,
+      "monday": 6,
+    }
+
+    await c1.find(query, {}).toArray(async (err, docs) => {
+      if (err) throw err;
+      console.log(docs);
+      //Check every journey entry
+      let modDocs = [];
+      for (let entry of docs) {
+        entry.UUID = [];
+        let service_days = entry.service_days[0];
+        //Get service days
+        for (let d of Object.keys(service_days)) {
+          if (service_days[d] === 1) {
+            for (let dt = new Date(2020, 11, (22 + offsets[d])); dt < new Date(2021, 0, 21); dt.setDate(dt.getDate() + 7)) {
+              entry.UUID.push(fixDate(dt) + "-" + entry.trip_id);
+            }
+          }
+        }
+        modDocs.push(entry);
+      }
+
+      console.log(modDocs);
+
+      await dbo.collection("final_trip_UUID_set").insertMany(modDocs, (err, results) => {
+        if (err) throw err;
+        console.log(results);
+        await dbo.collection("final_trip_UUID_set").createIndex({ "trip_id" : 1 }, { unique: true });
+      });
+    })
+  })
+  
+  app.get("/compare_schduled_to_observed", async (req, res) => {
+    let collection = dbo.collection("final_trip_UUID_set");
+    await collection.createIndex({ "trip_id" : 1 }, { unique: true });
+
+    let pipeline = [
+      {
+
+      }
+    ]
+    
+    let options = {
+      allowDiskUse: 1
+    };
+
+    collection.aggregate(pipeline, options).toArray((err, docs))
   })
   
   app.post('/postThat', (req, res) => {
     //code to perform particular action.
     //To access POST variable use req.body()methods.
     console.log(req.body);
-      for (let each of req.body) {
-        //find entry for trip
-        bulk.find({
-          "UUID": each.UUID
-        }).updateOne({
-          "$set": each
-        });
-        //Upsert entry for trip
-        bulk.find({
-          "UUID": each.UUID
-        }).upsert().updateOne({
-          "$setOnInsert": each
-        });
-      }
-      //Call execute
-      bulk.execute(function (err, updateResult) {
-        console.log(err, updateResult);
-        fs.appendFile('realtimeScript/RealtimeScriptLogs.txt',
-          new Date() + "\n" + "\tError:" + err + "\n" + "\tResults:\n" + "\t\tInserted: " + updateResult.nInserted + "\n" + "\t\tUpserted: " + updateResult.nUpserted + "\n" + "\t\tMatched: " + updateResult.nMatched + "\n" + "\t\tModified: " + updateResult.nModified + "\n" + "\t\tLastOp: " + updateResult.lastOp + "\n", (err) => {
-            if (err) throw err;
-          })
-        fs.appendFile('realtimeScript/RealtimeScriptLogs.txt',
-          "\n", (err) => {
-            if (err) throw err;
-          })
-      });
-    })
+  })
+
 })
 
 // add router in the Express app.
-// app.use("/postDat", router);
 
 app.listen(config.port, () => {
   console.log(`Express App running at http://localhost:${config.port}`);
 })
 
+/**
+ * Format date string as "YYYYMMDD"
+ * @param {Date} date 
+ */
+function fixDate(date) {
+  let monthString = date.getMonth() < 9 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
+  let dayString = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+  return date.getFullYear() + "" + monthString + "" + dayString;
+}
+
+/**
+ * Generate an array of dates between the start of our data and today
+ */
 function formDateArray() {
   let dates = [];
   for (let d = new Date(2020, 11, 22); d < new Date(); d.setDate(d.getDate() + 1)) {
