a0df76cbeb328d6dcbfdf8c93b1f6bd147880d1f
Chris Todd <chriswilltodd@gmail.com>
2021-01-18 14:11:58 +1300

    Change scheduleConfiguration to handle new versions. Change compare_UUID to compare correct date ranges. Closes #36

--- models/scheduleConfiguration.js
+++ models/scheduleConfiguration.js
@@ -4,63 +4,80 @@ const config = require('../config.js')
 const fetch = require('../node_modules/node-fetch');
 
 const MongoClient = require('../node_modules/mongodb').MongoClient;
-const client = new MongoClient(config.mongodb.uri, { useUnifiedTopology: true });
+const client = new MongoClient(config.testing.uri, { useUnifiedTopology: true });
 
 const fs = require('fs');
+
+let dbString = "ate_model_2";
 
 let urls = ["https://api.at.govt.nz/v2/gtfs/agency", 
             "https://api.at.govt.nz/v2/gtfs/calendar",
             "https://api.at.govt.nz/v2/gtfs/routes",
             "https://api.at.govt.nz/v2/gtfs/stops",
-            "https://api.at.govt.nz/v2/gtfs/trips",
-            "https://api.at.govt.nz/v2/gtfs/versions"];
+            "https://api.at.govt.nz/v2/gtfs/trips"];
 
 exec();
 
 async function exec() {
     client.connect(async (err, db) => {
       if (err) throw err;
-
-      //Build the exceptions collection
-      callTripUpdates("https://api.at.govt.nz/v2/gtfs/calendarDate").then((data) => {
-        saveDataLocally(data, "calendarDate");
-        postToMongo(db, data.response, "https://api.at.govt.nz/v2/gtfs/calendarDate");
+      let startObj;
+      // Get the versions to label the data we pull
+      callTripUpdates("https://api.at.govt.nz/v2/gtfs/versions").then((data) => {
+        startObj = data.response.reduce((res, obj) => {
+          return (obj.startdate < res.startdate) ? obj : res;
+        })
+        startObj = startObj.startdate.split("T")[0];
+        saveDataLocally(data, "versions", startObj);
+        postToMongo(db, data.response, "versions");
       })
-      await db.db("ate_model").collection("calendarDate").createIndex({"service_id" : 1});    
 
       //Build the other collections
       for (let url of urls) {
         let collectionName = url.split("/")[url.split("/").length - 1];
         let indexName = collectionName[collectionName.length - 1] === "s" ? `${collectionName.substring(0, collectionName.length - 1)}_id` : `${collectionName}_id`;
-        await db.db("ate_model").collection(collectionName).createIndex({indexName : 1}, {unique:true});
-        callTripUpdates(url).then(data=> {
-          saveDataLocally(data.response, collectionName);
+        await db.db(dbString).collection(collectionName).createIndex({ indexName: 1 }, { unique: true });
+        callTripUpdates(url).then(data => {
+          saveDataLocally(data.response, collectionName, startObj);
           postToMongo(db, data.response, collectionName);
         })
       }
-      //Close connection to Mongo
-      console.log("All data retrieved and posted to " + config.mongodb.uri + "! Goodbye :)");
-      db.close();
+
+      //Build the exceptions collection
+      callTripUpdates("https://api.at.govt.nz/v2/gtfs/calendarDate").then((data) => {
+        saveDataLocally(data, "calendarDate", startObj);
+        postToMongo(db, data.response, "calendarDate");
+      })
+      await db.db(dbString).collection("calendarDate").createIndex({ "service_id": 1 }).then();
     });
 }
 
-async function saveDataLocally(data, collectionName) {
+async function saveDataLocally(data, collectionName, version) {
   data = JSON.stringify(data);
+  if (!fs.existsSync("./dataBackups/" + version)) {
+    fs.mkdirSync("./dataBackups/" + version + "/");
+  }
   //Write to file in data folder for backups
-  fs.writeFileSync("./dataBackups/" + collectionName + "-" + new Date() + ".json", data, {flag: "+a"});
-  console.log(collectionName + " written to backups.")
+  try {
+    console.log("Attempting to overwrite existing file");
+    fs.writeFileSync("./dataBackups/" + version + "/" + collectionName + ".json", data);
+  } catch (err) {
+    console.log("File does not exist ¯\\_(ツ)_/¯, creating...");
+    fs.appendFileSync("./dataBackups/" + version + "/" + collectionName + ".json", data);
+  }
+  console.log(collectionName + " written to backups.");
 }
 
 async function postToMongo(db, data, collectionName) {
   console.log(collectionName + " received, posting to MongoDB collection " + collectionName + " at " + config.mongodb.uri);
 
   //Test for drop?
-  await db.db("ate_model").collection(collectionName).drop((err, results) => {
+  await db.db(dbString).collection(collectionName).drop((err, results) => {
     console.log(err, results);
   });
   //Insert new data
   //Beware of overlapping unique fields
-  await db.db("ate_model")
+  await db.db(dbString)
     .collection(collectionName)
     .insertMany(data, function (err, results) {
       if (err) throw err;
